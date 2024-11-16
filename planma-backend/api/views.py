40b1192e7aa5830e17c47fce997faae7f32b0665
CarreonJoshua <carreon.joshuad@gmail.com>
2024-11-16 14:09:13 +0800

    Added Events, Events Attended, Activity, and Activity Log tables as well as serializers and url

--- planma-backend/api/views.py
+++ planma-backend/api/views.py
@@ -4,8 +4,6 @@ from .models import *
 from .serializers import *
 from rest_framework.views import APIView
 
-
-
 class CustomTaskListCreateView(APIView):
     
     permission_classes = [permissions.AllowAny]
@@ -62,4 +60,234 @@ class CustomTaskUpdateView(APIView):
         else:
             return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
 
+#Below is the Events tables
+
+class CustomEventListCreateView(APIView):
     
+    permission_classes = [permissions.AllowAny]
+    def post(self, request):
+        data = request.data
+        serializer = CustomEventSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({**serializer.data}, status=status.HTTP_200_OK)
+        else:
+            return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+        # Set the student field to the authenticated user on creation
+        
+class CustomEventDetailView(APIView):
+   
+    permission_classes = [permissions.AllowAny]
+
+    def get(self,request,pk):
+        
+        user = CustomUser.objects.get(student_id = pk)
+        event = CustomEvents.objects.filter(student_id = user)
+        serializer = CustomEventSerializer(event, many = True)
+        
+        return Response(serializer.data)
+    
+class CustomEventDeleteView(APIView):
+
+    permission_classes = [permissions.AllowAny]
+    
+    def delete(self,request,pk):
+        try:
+            
+            deleteevent = CustomEvents.objects.get(event_id = pk)
+            
+            deleteevent.delete()
+            
+            return Response({"message : Post deleted Successfully"}, status=status.HTTP_200_OK)
+        except CustomEvents.DoesNotExist:
+            return Response({"error": "Post not found."}, status=status.HTTP_404_NOT_FOUND)
+
+class CustomEventUpdateView(APIView):
+
+    permission_classes = [permissions.AllowAny]
+    
+    def put(self, request, pk):
+        data= request.data
+        eventid= CustomEvents.objects.get(event_id = pk)
+        
+        serializer = CustomTaskSerializer(instance=eventid, data=data)
+        if serializer.is_valid():
+            
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+        
+#below is the Attended Event Table
+
+class AttendedEventListCreateView(APIView):
+    
+    permission_classes = [permissions.AllowAny]
+    def post(self, request):
+        data = request.data
+        serializer = AttendedEventSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({**serializer.data}, status=status.HTTP_200_OK)
+        else:
+            return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+        # Set the event_id field to the authenticated user on creation
+        
+class AttendedEventDetailView(APIView):
+   
+    permission_classes = [permissions.AllowAny]
+
+    def get(self,request,pk):
+        
+        event = CustomEvents.objects.get(event_id = pk)
+        attev = AttendedEvents.objects.filter(event_id = event)
+        serializer = AttendedEventSerializer(attev, many = True)
+        
+        return Response(serializer.data)
+    
+class AttendedEventDeleteView(APIView):
+
+    permission_classes = [permissions.AllowAny]
+    
+    def delete(self,request,pk):
+        try:
+            
+            deleteattev = CustomEvents.objects.get(att_events_id = pk)
+            
+            deleteattev.delete()
+            
+            return Response({"message : Post deleted Successfully"}, status=status.HTTP_200_OK)
+        except CustomEvents.DoesNotExist:
+            return Response({"error": "Post not found."}, status=status.HTTP_404_NOT_FOUND)
+
+class AttendedEventUpdateView(APIView):
+
+    permission_classes = [permissions.AllowAny]
+    
+    def put(self, request, pk):
+        data= request.data
+        attevid= CustomEvents.objects.get(att_events_id = pk)
+        
+        serializer = CustomTaskSerializer(instance=attevid, data=data)
+        if serializer.is_valid():
+            
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+
+#below is the activity tables
+
+class CustomActivityListCreateView(APIView):
+    
+    permission_classes = [permissions.AllowAny]
+    def post(self, request):
+        data = request.data
+        serializer = CustomActivitySerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({**serializer.data}, status=status.HTTP_200_OK)
+        else:
+            return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+        # Set the student_id field to the authenticated user on creation
+        
+class CustomActivityDetailView(APIView):
+   
+    permission_classes = [permissions.AllowAny]
+
+    def get(self,request,pk):
+        
+        user = CustomUser.objects.get(student_id = pk)
+        act = CustomActivity.objects.filter(student_id = user)
+        serializer = CustomActivitySerializer(act, many = True)
+        
+        return Response(serializer.data)
+    
+class CustomActivityDeleteView(APIView):
+
+    permission_classes = [permissions.AllowAny]
+    
+    def delete(self,request,pk):
+        try:
+            
+            deleteact = CustomActivity.objects.get(activity_id = pk)
+            
+            deleteact.delete()
+            
+            return Response({"message : Post deleted Successfully"}, status=status.HTTP_200_OK)
+        except CustomActivity.DoesNotExist:
+            return Response({"error": "Post not found."}, status=status.HTTP_404_NOT_FOUND)
+
+class CustomActivityUpdateView(APIView):
+
+    permission_classes = [permissions.AllowAny]
+    
+    def put(self, request, pk):
+        data= request.data
+        actid= CustomActivity.objects.get(activity_id = pk)
+        
+        serializer = CustomActivitySerializer(instance=actid, data=data)
+        if serializer.is_valid():
+            
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+
+#below is the Activity Log view
+
+class LogActivityListCreateView(APIView):
+    
+    permission_classes = [permissions.AllowAny]
+    def post(self, request):
+        data = request.data
+        serializer = ActivityLogSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({**serializer.data}, status=status.HTTP_200_OK)
+        else:
+            return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+        # Set the student_id field to the authenticated user on creation
+        
+class LogActivityDetailView(APIView):
+   
+    permission_classes = [permissions.AllowAny]
+
+    def get(self,request,pk):
+        
+        act = CustomActivity.objects.get(activity_id = pk)
+        actlog = ActivityLog.objects.filter(activity_id = act)
+        serializer = CustomActivitySerializer(actlog, many = True)
+        
+        return Response(serializer.data)
+    
+class LogActivityDeleteView(APIView):
+
+    permission_classes = [permissions.AllowAny]
+    
+    def delete(self,request,pk):
+        try:
+            
+            deleteactlog = ActivityLog.objects.get(act_log_id = pk)
+            
+            deleteactlog.delete()
+            
+            return Response({"message : Post deleted Successfully"}, status=status.HTTP_200_OK)
+        except ActivityLog.DoesNotExist:
+            return Response({"error": "Post not found."}, status=status.HTTP_404_NOT_FOUND)
+
+class LogActivityUpdateView(APIView):
+
+    permission_classes = [permissions.AllowAny]
+    
+    def put(self, request, pk):
+        data= request.data
+        actlogid= ActivityLog.objects.get(act_log_id = pk)
+        
+        serializer = ActivityLogSerializer(instance=actlogid, data=data)
+        if serializer.is_valid():
+            
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
