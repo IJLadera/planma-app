cb9a96a4ab4322289ac79be2473875c3cb7423cf
Zin <sanchezhanszin@gmail.com>
2025-10-19 15:47:54 +0800

    test #6 fixing fetchig and websocket

--- planma-backend/api/routing.py
+++ planma-backend/api/routing.py
@@ -1,5 +1,5 @@
 from django.urls import re_path
-from . import consumers
+from planmaDB import consumers
 
 websocket_urlpatterns = [
     re_path(r"ws/reminders/(?P<student_id>[\w-]+)/$", consumers.ReminderConsumer.as_asgi()),
